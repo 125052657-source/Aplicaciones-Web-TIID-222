3376de1eb916f7275609ad0e4de8588ea541def8
Francisco Joel Olvera Sánchez <125052657@upq.edu.mx>
2026-09-17 17:18:33 -0600

    Trabajamos con php y con estructura basica de html

---
+++ Index.php
@@ -0,0 +1,45 @@
+<?php
+    echo "Hola, perra"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+
+        h1 {
+            /*Texto*/
+            color: #ff0000;
+        }
+        div {
+            /*Fonfo*/
+            background: #ff4800;
+        }
+        
+
+
+    </style>
+</head>
+<body>
+    
+    <h1>Titulo</h1>
+    <h1>Hola, perra</h1>
+    <h2>Hola, perra</h2>
+    <h3>Hola, perra</h3>
+    <h4>Hola, perra</h4>
+    <h5>Hola, perra</h5>
+    <h6>Hola, perra</h6>
+
+    <div>
+        <h2>Titulo del párrafo</h2>
+
+        <p>Este es el párrafo</p>
+
+    </div>
+
+
+</body>
+</html>
