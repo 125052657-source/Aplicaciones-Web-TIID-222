86e5371cc353b7804f8a63574dfdb7b6f8b0f774
Francisco Joel Olvera Sánchez <125052657@upq.edu.mx>
2026-09-18 19:01:14 -0600

    Se trabajo en los estilos y en div

--- Index.php
+++ Index.php
@@ -14,11 +14,33 @@
             /*Texto*/
             color: #ff0000;
         }
-        div {
+        .seccion {
+
             /*Fonfo*/
-            background: #ff4800;
+            background: #00b7ff;
+            padding: 20px 40px;
+            margin: 20px;
+            border: 20px Double black;
+
         }
-        
+        .seccion1 {
+            /*Fonfo*/
+            background: #ff0062;
+
+            padding-top: 20px; /* → Arriba */
+            padding-right: 40px; /* → Derecha */
+            padding-bottom: 20px; /* → Abajo */
+            padding-left: 40px; /* → Izquierda */
+
+            margin-top: 20px; /* → Arriba */
+            margin-right: 30px; /* → Derecha */
+            margin-bottom: 40px; /* → Abajo */
+            margin-left: 50px; /* → Izquierda */
+
+
+
+        }
+
 
 
     </style>
@@ -33,10 +55,66 @@
     <h5>Hola, perra</h5>
     <h6>Hola, perra</h6>
 
-    <div>
-        <h2>Titulo del párrafo</h2>
+    <div class="seccion">
+        <h2>Volando Remix</h2>
 
-        <p>Este es el párrafo</p>
+        <p>Disfruto el poder verte aunque no sea conmigo, no
+            Lo nuestro e'algo aparte y la cama fue testigo
+            De toa esa' noche', de to' eso' gemido'
+            Todavía escucho tu voz cerquita de mi oído
+            Má' a'lante vive gente, pero no me he movido
+            Sigo estancao, en tus recuerdo' perdío
+            El DM explotao, pero la dejo en leído
+            Mi' letra' siempre tienen tu nombrе y apellido
+            Viviendo con mil pregunta', el lápiz sin punta
+            Dе todo lo que escribo, ma, tú tiene' la culpa
+            Llevo años pagando la misma multa
+            Tú ere' ese mal que no sé por qué me gusta
+            El no poder olvidarte me frustra
+            Te lo juro que me frustra
+            Pero si me llama' le llego volando
+            En la Mercede' voy capsuliando
+            Y cuando no está', te sigo imaginando
+            Sin ropa en mi cama, mi nombre gritando
+            Y si me llama' le llego volando
+            En la Mercede' voy capsuliando
+            Y cuando no está', te sigo imaginando
+            Sin ropa en mi cama, mi nombre gritando, oh
+            Y si me llama yo le caigo en el AMG
+            Baby, pa terminar lo que a mitad dejé
+            En la cama hicimos una serie pero no PG
+            Y aunque me alejé</p>
+
+    </div>
+    <div class="seccion1">
+        <h2>Volando Remix</h2>
+
+        <p>Disfruto el poder verte aunque no sea conmigo, no
+            Lo nuestro e'algo aparte y la cama fue testigo
+            De toa esa' noche', de to' eso' gemido'
+            Todavía escucho tu voz cerquita de mi oído
+            Má' a'lante vive gente, pero no me he movido
+            Sigo estancao, en tus recuerdo' perdío
+            El DM explotao, pero la dejo en leído
+            Mi' letra' siempre tienen tu nombrе y apellido
+            Viviendo con mil pregunta', el lápiz sin punta
+            Dе todo lo que escribo, ma, tú tiene' la culpa
+            Llevo años pagando la misma multa
+            Tú ere' ese mal que no sé por qué me gusta
+            El no poder olvidarte me frustra
+            Te lo juro que me frustra
+            Pero si me llama' le llego volando
+            En la Mercede' voy capsuliando
+            Y cuando no está', te sigo imaginando
+            Sin ropa en mi cama, mi nombre gritando
+            Y si me llama' le llego volando
+            En la Mercede' voy capsuliando
+            Y cuando no está', te sigo imaginando
+            Sin ropa en mi cama, mi nombre gritando, oh
+            Y si me llama yo le caigo en el AMG
+            Baby, pa terminar lo que a mitad dejé
+            En la cama hicimos una serie pero no PG
+            Y aunque me alejé</p>
 
     </div>
 
